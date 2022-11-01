f104e6daf4bda85df258c0ff19d76dbb9cc30f0f
alexwood4real <akwood6@asu.edu>
2022-11-01 15:06:04 -0700

    Initial Commit

---
+++ Social Sport/Social_SportApp.swift
@@ -0,0 +1,17 @@
+//
+//  Social_SportApp.swift
+//  Social Sport
+//
+//  Created by OG Brown Skin on 11/1/22.
+//
+
+import SwiftUI
+
+@main
+struct Social_SportApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
