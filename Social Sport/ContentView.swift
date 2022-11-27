befb200ff9f9f77daae70e68702b9e9f743d9854
alexwood4real <akwood6@asu.edu>
2022-11-26 17:40:49 -0700

    Update app icon and begin onboarding process

--- Social Sport/ContentView.swift
+++ Social Sport/ContentView.swift
@@ -7,13 +7,29 @@
 
 import SwiftUI
 
+
 struct ContentView: View {
+    @State private var username = ""
+    @State private var password = ""
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
+        ZStack{
+            Image("SS_logo_green")
+                .scaledToFit()
                 .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            VStack(alignment: .leading, spacing: 15) {
+                TextField("Username", text: self.$username)
+                    .padding()
+                    .cornerRadius(20.0)
+                SecureField("Password", text: self.$password)
+                    .padding()
+                    .cornerRadius(20.0)
+                Button("Sign In"){}
+                    .padding()
+            }
+            .scaledToFit()
+            .padding()
+            // Button("Sign In"){}
+            // .scaledToFit()
         }
         .padding()
     }
